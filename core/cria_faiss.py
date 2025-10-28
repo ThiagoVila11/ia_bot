2930859f52d18743ae57dfdd49e14969978486df
ThiagoVila11 <simbiox.thiago.tosatti@vila11.com.br>
2025-10-28 14:15:53 -0300

    Update cria_faiss.py

--- core/cria_faiss.py
+++ core/cria_faiss.py
@@ -3,22 +3,18 @@ from pathlib import Path
 import os
 import django
 
-# 1. Adiciona o diretório do projeto ao PYTHONPATH
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 
-# 2. Configura Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ia_bot.settings")
 django.setup()
 
-# 3. Agora pode importar modelos e bibliotecas
 from core.models import Parametro
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader, DirectoryLoader, CSVLoader
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 
-# 4. Busca chave da OpenAI do banco
 try:
     openai_key = Parametro.objects.get(parametroChave='OPENAI_API_KEY').parametroValor
 except Parametro.DoesNotExist:
@@ -35,7 +31,6 @@ loader_txt = DirectoryLoader(
 )
 docs_txt = loader_txt.load()
 
-# Carregar arquivos .csv
 loader_csv = DirectoryLoader(
     path="meus_arquivos",
     glob="**/*.csv",
@@ -44,14 +39,11 @@ loader_csv = DirectoryLoader(
 )
 docs_csv = loader_csv.load()
 
-# Juntar todos os documentos
 documents = docs_txt + docs_csv
 
-# 6. Split
 splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 docs = splitter.split_documents(documents)
 
-# 7. Embeddings + FAISS
 embeddings = OpenAIEmbeddings(openai_api_key=openai_key)
 db = FAISS.from_documents(docs, embeddings)
 db.save_local("vector_index")
